88ba301f4b586186593122333bc26a490c8c3557
Jean Bosco Mugiraneza <mjeanbosco19@gmail.com>
2026-01-01 17:06:44 +0200

    Wow! Part 1 of Assignement is Complete, and is working
    - Introduced CarTrackerApplication as the main entry point for the Spring Boot application.
    - Added Maven Compiler Plugin configuration to ensure compatibility with the specified Java version.
    - Updated CarController to use a more explicit PathVariable annotation.
    - Enhanced application properties with logging levels and Jackson serialization settings for improved output formatting.

---
+++ backend/src/main/java/com/codehills/cartracker/CarTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.codehills.cartracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarTrackerApplication.class, args);
+    }
+}
